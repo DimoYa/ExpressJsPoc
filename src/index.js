adf3f55d78932b04f415b269e359e0c10cc15dbb
Dimo Yanev <Dimo_Yanev@abv.bg>
2022-02-01 17:45:00 +0200

    connect db and add new cars data

--- src/index.js
+++ src/index.js
@@ -1,41 +1,47 @@
-const express = require('express');
-const hbs = require('express-handlebars');
-const { home } = require('./controllers/home');
-const { about } = require('./controllers/about');
-const create = require('./controllers/create');
-const { details } = require('./controllers/details');
-const { notFound } = require('./controllers/notFound');
-const carService = require('./services/car');
-const _delete = require('./controllers/delete');
-const edit = require('./controllers/edit');
+const express = require("express");
+const hbs = require("express-handlebars");
 
-const app = express();
+const initDB = require("./models/index");
 
-app.use(express.urlencoded({ extended: true }));
-app.use('/static', express.static('static'));
-app.use(carService());
+const { home } = require("./controllers/home");
+const { about } = require("./controllers/about");
+const create = require("./controllers/create");
+const { details } = require("./controllers/details");
+const { notFound } = require("./controllers/notFound");
+const carService = require("./services/car");
+const _delete = require("./controllers/delete");
+const edit = require("./controllers/edit");
 
-app.engine('hbs', hbs.create({
-    extname: '.hbs'
-}).engine);
-app.set('view engine', 'hbs');
+start();
 
-app.get('/', home);
-app.get('/about', about);
-app.get('/details/:id', details);
+async function start() {
+  await initDB();
 
-app.route('/create')
-    .get(create.get)
-    .post(create.post);
+  const app = express();
 
-app.route('/delete/:id')
-    .get(_delete.get)
-    .post(_delete.post);
+  app.use(express.urlencoded({ extended: true }));
+  app.use("/static", express.static("static"));
+  app.use(carService());
 
-app.route('/edit/:id')
-    .get(edit.get)
-    .post(edit.post);
+  app.engine(
+    "hbs",
+    hbs.create({
+      extname: ".hbs",
+    }).engine
+  );
+  app.set("view engine", "hbs");
 
-app.all('*', notFound);
+  app.get("/", home);
+  app.get("/about", about);
+  app.get("/details/:id", details);
 
-app.listen(3000, () => console.log('app startеd'));
+  app.route("/create").get(create.get).post(create.post);
+
+  app.route("/delete/:id").get(_delete.get).post(_delete.post);
+
+  app.route("/edit/:id").get(edit.get).post(edit.post);
+
+  app.all("*", notFound);
+
+  app.listen(3000, () => console.log("app startеd"));
+}
---
+++ src/models/index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+require('./car');
+
+const connectionString = "mongodb://localhost:27017/carbicle";
+
+async function init() {
+  try {
+    await mongoose.connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.log(err);
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+module.exports = init;
